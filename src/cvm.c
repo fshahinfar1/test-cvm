687a7911dc898e633be77fb14394baa0fa1df686
Farbod Shahinfar <fshahinfar1@gmail.com>
2025-03-14 13:39:42 +0100

    implement cvm algorithm

--- src/cvm.c
+++ src/cvm.c
@@ -8,11 +8,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
+#include <time.h>
 
 // The buffer size we want to use for CVM (|B| in the Knuth's CVM paper)
 #define TREAP_MAX_SIZE 128
-#define TREAP_MAX_HEIGHT 16
+#define TREAP_MAX_HEIGHT 32
 
+#include "treap/test/treap_test.h"
 #include "treap/treap.h"
 #include "fixed_point/fp.h"
 
@@ -24,31 +26,80 @@ struct cvm_impl {
 struct cvm_impl *cvm_new()
 {
 	// allocating the CVM struct and treap struct
-	size_t sz = sizeof(struct cvm_impl) + sizeof(struct treap);
-	struct cvm_impl *c = calloc(1, sz);
-	if (c == NULL)
+	struct cvm_impl *c = calloc(1, sizeof(struct cvm_impl));
+	if (c == NULL) return NULL;
+	c->t = treap_new();
+	if (c->t == NULL) {
+		free(c);
 		return NULL;
-	c->t = (struct treap *)(c + 1);
+	}
 	c->p = FP_ONE;
 	return c;
 }
 
 uint64_t cvm_estimate(struct cvm_impl *cvm)
 {
-	return cvm->t->used * fp_recerpical_to_uint(cvm->p);
+	return (uint64_t)((double)cvm->t->used / fp_to_float(cvm->p));
 }
 
 int main(int argc, char *argv[])
 {
-	struct cmv_impl *cvm = cvm_new();
+	srand(time(0));
+	struct cvm_impl *cvm = cvm_new();
+	int ret;
 
 	// read the stream of keys from a file
 	FILE *f = fopen("./test/data.txt", "r");
 	assert(f != NULL);
 	struct treap_key key;
-	int line_counter = 0;
+	/* int line_counter = 0; */
 	while (fscanf(f, "%d", (int *)&key.data) > 0) {
-		printf("%d: %d\n", line_counter++, *(int *)key.data);
+		/* printf("%d: %d\n", line_counter++, *(int *)key.data); */
+
+		// if we have the element in the buffer, remove it
+		ret = treap_delete(cvm->t, &key);
+		if (ret == 0) {
+			printf("delete key: %d\n", *(uint32_t *)key.data);
+		}
+		fp_t u = fp_random();
+		/* printf("rand: %f | %f\n", fp_to_float(u), 1/fp_to_float(u)); */
+		if (u >= cvm->p)
+			continue;
+		if (treap_has_space(cvm->t)) {
+			ret = treap_insert(cvm->t, &key, u);
+			assert(ret == 0);
+			continue;
+		}
+		// u < p and |B| = s
+		assert(u < cvm->p);
+		assert(cvm->t->used == TREAP_MAX_SIZE);
+		struct treap_node *top = treap_top(cvm->t);
+		if (u > top->priority) {
+			/* printf("(1) p: %f --> %f\n", fp_to_float(cvm->p), fp_to_float(u)); */
+			cvm->p = u;
+		} else {
+			/* printf("(2) p: %f --> %f\n", fp_to_float(cvm->p), fp_to_float(top->priority)); */
+			cvm->p = top->priority;
+			// TODO: I may be able to implement a better node replace code than remove and add
+			/* printf("old top key: %d (%f)\n", *(uint32_t *)&top->key, fp_to_float(top->priority)); */
+			/* top->priority = u; */
+			/* memcpy(top->key.data, key.data, TREAP_KEY_SIZE); */
+			/* printf("new top key: %d (%f)\n", *(uint32_t *)&top->key, fp_to_float(top->priority)); */
+
+			ret = treap_delete(cvm->t, &top->key);
+			assert(ret == 0);
+			if(!treap_valid(cvm->t->root)) {
+				assert(0);
+			}
+
+			ret = treap_insert(cvm->t, &key, u);
+			if (ret == -2)
+				printf("height issue: %d\n", ret);
+			assert(ret == 0);
+			assert(treap_valid(cvm->t->root) == 1);
+		}
 	}
+	printf("|B|: %d   p: %f\n", cvm->t->used, fp_to_float(cvm->p));
+	printf("Estimate: %ld\n", cvm_estimate(cvm));
 	return 0;
 }
